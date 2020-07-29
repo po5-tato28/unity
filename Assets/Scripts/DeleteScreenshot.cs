4eaa5e99084381e5542ecb9b8a74e72837ba33a9
po5-tato28 <65820373+po5-tato28@users.noreply.github.com>
2020-07-29 15:27:06 +0900

    3rd - Firebase Storage (3)
    https://www.notion.so/po5tato28/2020-07-29-Firebase-Storage-3-79d35daea61d4f1e90e5f8b6a07819fd

---
+++ Assets/Scripts/DeleteScreenshot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Firebase.Storage;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DeleteScreenshot : MonoBehaviour
+{
+    public void Trigger(string path)
+    {
+        StartCoroutine(DeleteScreenshotCoroutine(path));
+    }
+
+    private IEnumerator DeleteScreenshotCoroutine(string path)
+    {
+        var storage = FirebaseStorage.DefaultInstance;
+        // 테스트할 때마다 경로 바꿔서 테스트 가능
+        var screenshotReference = storage.GetReference(path).Child($"/screenshots/44799135-88f8-4350-b473-0757ea3a97e8.png");
+        
+        var deleteTask = screenshotReference.DeleteAsync();
+        yield return new WaitUntil(() => deleteTask.IsCompleted);
+        if (deleteTask.Exception != null)
+        {
+            Debug.LogError($"삭제에 실패했습니다. {deleteTask.Exception}");
+            yield break;
+        }
+        else
+        {
+            Debug.Log("삭제 완료" + screenshotReference);
+        }
+
+        yield return null;
+    }
+}
